991e62931d972bc090a9f56d2efe20f76d54d528
Alan Pasho <apasho@gmail.com>
2014-07-27 11:02:43 -0300

    Read me text edited

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
  conditional meta box - by Alan Pasho, wordpressdev.net
  
-the meta box will appear when a specific template is in use
+the meta box will appear when a specific template is in use.
  add this code to your functions.php and change 'my-template_name.php' to the name
  of your template file.
--- add_to_funtions.php
+++ add_to_funtions.php
@@ -2,7 +2,7 @@
 
 /*
  * conditional meta box - by Alan Pasho, wordpressdev.net
- * the meta box will appear when a specific template is in use
+ * the meta box will appear when a specific template is in use.
  * add this code to your functions.php and change 'my-template_name.php' to the name
  * of your template file.
  */
